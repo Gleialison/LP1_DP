2db318942982b1277921e3f43659567a8187c170
Gleialison <127407369+Gleialison@users.noreply.github.com>
2024-02-28 00:31:28 -0300

    Exercício 01, linguagem de programação

---
+++ EX_1.java
@@ -0,0 +1,12 @@
+package linguagem_programacao;
+
+public class EX_1 {
+
+	public static void main(String[] args) {
+    System.out.println("Olá Mundo");
+    System.out.println("Tudo bem com voce!");
+    System.out.println("Voce foi trabalhar hoje? \nSim, eu fui!");
+
+	}
+
+}
